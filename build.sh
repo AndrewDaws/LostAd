b6a15a856701fc9a53b27ce94a5b18140f3ca967
Lenni Hein <git@lennihein.com>
2022-06-24 15:28:34 +0200

    made compatible with abp

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ git pull
 hostlist-compiler -c lostad.json -o lostad.txt
 hostlist-compiler -c lostad_dns.json -o lostad_dns.txt
 sed -i '/^$/d' lostad*.txt
+echo -e "[Adblock Plus 2.0]\n$(cat lostad.txt)" > lostad.txt
 git stage lost*
 git commit -m "Build $(timestamp) UTC"
 git push
