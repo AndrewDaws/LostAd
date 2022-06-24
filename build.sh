0a667b5cccb94b324be749de3805b2e539395582
Lenni Hein <git@lennihein.com>
2022-06-24 11:54:15 +0000

    fixed build script

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
+timestamp() {
+  date +"%Y/%m/%d %H:%M:%S"
+}
 git pull
 hostlist-compiler -c lostad.json -o lostad.txt
 hostlist-compiler -c lostad_dns.json -o lostad_dns.txt
 sed -i '/^$/d' lostad*.txt
 git stage lost*
-git commit -m "$(TIMESTAMP)"
+git commit -m "Build $(timestamp)"
 git push
