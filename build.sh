0170f7efa55a767a8a5009c30299e30ffc33bd25
Lenni Hein <git@lennihein.com>
2022-06-24 14:09:13 +0200

    added UTC

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ hostlist-compiler -c lostad.json -o lostad.txt
 hostlist-compiler -c lostad_dns.json -o lostad_dns.txt
 sed -i '/^$/d' lostad*.txt
 git stage lost*
-git commit -m "Build $(timestamp)"
+git commit -m "Build $(timestamp) UTC"
 git push
