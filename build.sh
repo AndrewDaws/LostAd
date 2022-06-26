da67a558e31f586e4b124dcbe49550e2c97bd70e
Lenni Hein <git@lennihein.com>
2022-06-26 16:05:44 +0000

    Build 2022/06/26 16:05:44 UTC

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ git pull
 hostlist-compiler -c lostad.json -o lostad.txt
 hostlist-compiler -c lostad_dns.json -o lostad_dns.txt
 sed -i '/^$/d' lostad*.txt
+echo -e "! Expires: 1 day\n$(cat lostad.txt)" > lostad.txt
+echo -e "! Expires: 1 day\n$(cat lostad_dns.txt)" > lostad_dns.txt
 echo -e "[Adblock Plus 2.0]\n$(cat lostad.txt)" > lostad.txt
 git stage lost*
 git commit -m "Build $(timestamp) UTC"
